949e03a7e035c10b0cb4eb3546c29499769b0771
Tino Hager <tino.hager@nager.at>
2024-12-05 12:14:43 +0100

    Update DmarcRecordParserTest.cs

--- src/Nager.MailAuth.UnitTest/DmarcRecordParserTest.cs
+++ src/Nager.MailAuth.UnitTest/DmarcRecordParserTest.cs
@@ -4,11 +4,37 @@
     public sealed class DmarcRecordParserTest
     {
         [TestMethod]
-        public void TryParse_ValidDmarcString_ReturnsTrueAndPopulatesDmarcRecord()
+        public void TryParse_InvalidDmarcString1_ReturnsTrueAndPopulatesDmarcRecord()
         {
             var isSuccessful = DmarcRecordParser.TryParse("v=DMARC", out var dmarcRecord);
             Assert.IsTrue(isSuccessful);
             Assert.IsNotNull(dmarcRecord);
+        }
+
+        [TestMethod]
+        public void TryParse_InvalidDmarcString2_ReturnsTrueAndPopulatesDmarcRecord()
+        {
+            var isSuccessful = DmarcRecordParser.TryParse("v=DMARC1", out var dmarcRecord);
+            Assert.IsTrue(isSuccessful);
+            Assert.IsNotNull(dmarcRecord);
+        }
+
+        [TestMethod]
+        public void TryParse_InvalidDmarcString3_ReturnsTrueAndPopulatesDmarcRecord()
+        {
+            var isSuccessful = DmarcRecordParser.TryParse("v=DMARC1; p=Test", out var dmarcRecord);
+            Assert.IsTrue(isSuccessful);
+            Assert.IsNotNull(dmarcRecord);
+            Assert.AreEqual("Test", dmarcRecord.DomainPolicy);
+        }
+
+        [TestMethod]
+        public void TryParse_InvalidDmarcString4_ReturnsTrueAndPopulatesDmarcRecord()
+        {
+            var isSuccessful = DmarcRecordParser.TryParse("v=DMARC1; p=Test;", out var dmarcRecord);
+            Assert.IsTrue(isSuccessful);
+            Assert.IsNotNull(dmarcRecord);
+            Assert.AreEqual("Test", dmarcRecord.DomainPolicy);
         }
     }
 }
