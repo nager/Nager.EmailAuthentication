0ce44c2b28c8587c89f35540e45e99f785265a87
Tino Hager <tino.hager@nager.at>
2024-12-06 08:04:35 +0100

    add more unit tests

--- src/Nager.EmailAuthentication.UnitTest/DmarcRecordParserTest.cs
+++ src/Nager.EmailAuthentication.UnitTest/DmarcRecordParserTest.cs
@@ -6,35 +6,79 @@
         [TestMethod]
         public void TryParse_InvalidDmarcString1_ReturnsTrueAndPopulatesDmarcRecord()
         {
-            var isSuccessful = DmarcRecordParser.TryParse("v=DMARC", out var dmarcDataFragment);
+            var isSuccessful = DmarcRecordParser.TryParse("v=DMARC", out var dmarcDataFragment, out var unrecognizedParts);
             Assert.IsTrue(isSuccessful);
             Assert.IsNotNull(dmarcDataFragment);
+            Assert.IsNull(unrecognizedParts);
         }
 
         [TestMethod]
         public void TryParse_InvalidDmarcString2_ReturnsTrueAndPopulatesDmarcRecord()
         {
-            var isSuccessful = DmarcRecordParser.TryParse("v=DMARC1", out var dmarcDataFragment);
+            var isSuccessful = DmarcRecordParser.TryParse("v=DMARC1", out var dmarcDataFragment, out var unrecognizedParts);
             Assert.IsTrue(isSuccessful);
             Assert.IsNotNull(dmarcDataFragment);
+            Assert.IsNull(unrecognizedParts);
         }
 
         [TestMethod]
         public void TryParse_InvalidDmarcString3_ReturnsTrueAndPopulatesDmarcRecord()
         {
-            var isSuccessful = DmarcRecordParser.TryParse("v=DMARC1; p=Test", out var dmarcDataFragment);
+            var isSuccessful = DmarcRecordParser.TryParse("v=DMARC1; p=Test", out var dmarcDataFragment, out var unrecognizedParts);
             Assert.IsTrue(isSuccessful);
             Assert.IsNotNull(dmarcDataFragment);
             Assert.AreEqual("Test", dmarcDataFragment.DomainPolicy);
+            Assert.IsNull(unrecognizedParts);
         }
 
         [TestMethod]
         public void TryParse_InvalidDmarcString4_ReturnsTrueAndPopulatesDmarcRecord()
         {
-            var isSuccessful = DmarcRecordParser.TryParse("v=DMARC1; p=Test;", out var dmarcDataFragment);
+            var isSuccessful = DmarcRecordParser.TryParse("v=DMARC1; p=Test;", out var dmarcDataFragment, out var unrecognizedParts);
             Assert.IsTrue(isSuccessful);
             Assert.IsNotNull(dmarcDataFragment);
             Assert.AreEqual("Test", dmarcDataFragment.DomainPolicy);
+            Assert.IsNull(unrecognizedParts);
+        }
+
+        [TestMethod]
+        public void TryParse_ValidDmarcString1_ReturnsTrueAndPopulatesDmarcRecord()
+        {
+            var isSuccessful = DmarcRecordParser.TryParse("v=DMARC1; p=reject;", out var dmarcDataFragment, out var unrecognizedParts);
+            Assert.IsTrue(isSuccessful);
+            Assert.IsNotNull(dmarcDataFragment);
+            Assert.AreEqual("reject", dmarcDataFragment.DomainPolicy);
+            Assert.IsNull(unrecognizedParts);
+        }
+
+        [TestMethod]
+        public void TryParse_ValidDmarcString2_ReturnsTrueAndPopulatesDmarcRecord()
+        {
+            var isSuccessful = DmarcRecordParser.TryParse("v=DMARC1; p=reject; sp=none;", out var dmarcDataFragment, out var unrecognizedParts);
+            Assert.IsTrue(isSuccessful);
+            Assert.IsNotNull(dmarcDataFragment);
+            Assert.AreEqual("reject", dmarcDataFragment.DomainPolicy);
+            Assert.AreEqual("none", dmarcDataFragment.SubdomainPolicy);
+            Assert.IsNull(unrecognizedParts);
+        }
+
+        [TestMethod]
+        public void TryParse_CorruptDmarcString1_ReturnsTrueAndPopulatesDmarcRecord()
+        {
+            var isSuccessful = DmarcRecordParser.TryParse("verification=123456789", out var dmarcDataFragment, out var unrecognizedParts);
+            Assert.IsTrue(isSuccessful);
+            Assert.IsNotNull(dmarcDataFragment);
+            Assert.IsNotNull(unrecognizedParts);
+            Assert.IsTrue(unrecognizedParts.Length == 1);
+        }
+
+        [TestMethod]
+        public void TryParse_CorruptDmarcString2_ReturnsTrueAndPopulatesDmarcRecord()
+        {
+            var isSuccessful = DmarcRecordParser.TryParse(" ", out var dmarcDataFragment, out var unrecognizedParts);
+            Assert.IsFalse(isSuccessful);
+            Assert.IsNull(dmarcDataFragment);
+            Assert.IsNull(unrecognizedParts);
         }
     }
 }
