85cf1a89d30fd4d5cffa21f600ce73bcc627b001
Tino Hager <tino.hager@nager.at>
2025-01-16 16:36:05 +0100

    add dkim header parser (wip)

---
+++ src/Nager.EmailAuthentication/DkimHeaderParser.cs
@@ -0,0 +1,169 @@
+﻿using Nager.EmailAuthentication.Models;
+
+namespace Nager.EmailAuthentication
+{
+    public static class DkimHeaderParser
+    {
+        public static bool TryParse(
+            string dkimHeader,
+            out DkimHeaderDataFragment? dkimHeaderDataFragment)
+        {
+            return TryParse(dkimHeader, out dkimHeaderDataFragment, out _);
+        }
+
+        public static bool TryParse(
+            string dkimHeader,
+            out DkimHeaderDataFragment? dkimHeaderDataFragment,
+            out ParseError[]? parseErrors)
+        {
+            parseErrors = null;
+
+            var errors = new List<ParseError>();
+
+            if (string.IsNullOrWhiteSpace(dkimHeader))
+            {
+                dkimHeaderDataFragment = null;
+                return false;
+            }
+
+            var keyValueSeperator = '=';
+            var keyValueParser = new KeyValueParser.MemoryEfficientKeyValueParser(';', keyValueSeperator);
+            if (!keyValueParser.TryParse(dkimHeader, out var parseResult))
+            {
+                dkimHeaderDataFragment = null;
+                return false;
+            }
+
+            if (parseResult == null)
+            {
+                dkimHeaderDataFragment = null;
+                return false;
+            }
+
+            var duplicateConfigurations = parseResult.KeyValues
+                .GroupBy(o => o.Key)
+                .Where(g => g.Count() > 1);
+
+            foreach (var duplicate in duplicateConfigurations)
+            {
+                errors.Add(new ParseError
+                {
+                    Severity = ErrorSeverity.Error,
+                    ErrorMessage = $"Duplicate configuration detected for key: '{duplicate.Key}'."
+                });
+            }
+
+            var dataFragment = new DkimHeaderDataFragment();
+
+            var handlers = new Dictionary<string, MappingHandler>
+            {
+                {
+                    "v", new MappingHandler
+                    {
+                        Map = value => dataFragment.Version = value
+                    }
+                },
+                {
+                    "a", new MappingHandler
+                    {
+                        Map = value => dataFragment.SignatureAlgorithm = value
+                    }
+                },
+                {
+                    "b", new MappingHandler
+                    {
+                        Map = value => dataFragment.SignatureData = value
+                    }
+                },
+                {
+                    "bh", new MappingHandler
+                    {
+                        Map = value => dataFragment.BodyHash = value
+                    }
+                },
+                {
+                    "c", new MappingHandler
+                    {
+                        Map = value => dataFragment.MessageCanonicalization = value
+                    }
+                },
+                {
+                    "d", new MappingHandler
+                    {
+                        Map = value => dataFragment.Domain = value
+                    }
+                },
+                {
+                    "s", new MappingHandler
+                    {
+                        Map = value => dataFragment.Selector = value
+                    }
+                },
+                {
+                    "t", new MappingHandler
+                    {
+                        Map = value => dataFragment.Timestamp = value
+                    }
+                },
+                {
+                    "x", new MappingHandler
+                    {
+                        Map = value => dataFragment.SignatureExpiration = value
+                    }
+                },
+                {
+                    "h", new MappingHandler
+                    {
+                        Map = value => dataFragment.SignedHeaderFields = value
+                    }
+                },
+                {
+                    "q", new MappingHandler
+                    {
+                        Map = value => dataFragment.QueryMethods = value
+                    }
+                },
+                {
+                    "i", new MappingHandler
+                    {
+                        Map = value => dataFragment.AgentOrUserIdentifier = value
+                    }
+                }
+            };
+
+            var mappingFound = false;
+
+            foreach (var keyValue in parseResult.KeyValues)
+            {
+                if (string.IsNullOrEmpty(keyValue.Key))
+                {
+                    continue;
+                }
+
+                if (handlers.TryGetValue(keyValue.Key.ToLowerInvariant(), out var handler))
+                {
+                    if (handler.Validate != null)
+                    {
+                        errors.AddRange([.. handler.Validate(new ValidateRequest { Field = keyValue.Key, Value = keyValue.Value })]);
+                    }
+                    handler.Map(keyValue.Value ?? "");
+
+                    mappingFound = true;
+
+                    continue;
+                }
+
+                errors.Add(new ParseError
+                {
+                    ErrorMessage = $"Unrecognized Part {keyValue.Key}{keyValueSeperator}{keyValue.Value}",
+                    Severity = ErrorSeverity.Warning
+                });
+            }
+
+            parseErrors = errors.Count == 0 ? null : [.. errors];
+            dkimHeaderDataFragment = dataFragment;
+
+            return mappingFound;
+        }
+    }
+}
---
+++ src/Nager.EmailAuthentication/Models/DkimHeaderDataFragment.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nager.EmailAuthentication.Models
+{
+    public class DkimHeaderDataFragment
+    {
+        /// <summary>
+        /// Dkim Version <strong>(v=)</strong>
+        /// </summary>
+        public string? Version { get; set; }
+
+        /// <summary>
+        /// Dkim Signature Algorithm <strong>(a=)</strong>
+        /// </summary>
+        public string? SignatureAlgorithm { get; set; }
+
+        /// <summary>
+        /// Signature data <strong>(b=)</strong>
+        /// </summary>
+        public string? SignatureData { get; set; }
+
+        /// <summary>
+        /// Body hash <strong>(bh=)</strong>
+        /// </summary>
+        public string? BodyHash { get; set; }
+
+        /// <summary>
+        /// Message canonicalization <strong>(c=)</strong>
+        /// </summary>
+        public string? MessageCanonicalization { get; set; }
+
+        /// <summary>
+        /// Domain <strong>(d=)</strong>
+        /// </summary>
+        public string? Domain { get; set; }
+
+        /// <summary>
+        /// Selector <strong>(s=)</strong>
+        /// </summary>
+        public string? Selector { get; set; }
+
+        /// <summary>
+        /// Signature Timestamp <strong>(t=)</strong>
+        /// </summary>
+        public string? Timestamp { get; set; }
+
+        /// <summary>
+        /// Signature Expiration <strong>(x=)</strong>
+        /// </summary>
+        public string? SignatureExpiration { get; set; }
+
+        /// <summary>
+        /// Signed header fields <strong>(h=)</strong>
+        /// </summary>
+        public string? SignedHeaderFields { get; set; }
+
+        /// <summary>
+        /// Query Methods, default is "dns/txt" <strong>(q=)</strong>
+        /// </summary>
+        public string? QueryMethods { get; set; }
+
+        /// <summary>
+        /// The Agent or User Identifier <strong>(i=)</strong>
+        /// </summary>
+        public string? AgentOrUserIdentifier { get; set; }
+    }
+}
