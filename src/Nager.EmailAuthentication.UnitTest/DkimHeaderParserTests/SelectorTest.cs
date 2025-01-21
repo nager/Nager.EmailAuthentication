af233b7c23ec41872ee76630b8b2f01d4297e47b
Tino Hager <tino.hager@nager.at>
2025-01-21 15:13:10 +0100

    Add unit tests for dkim selector

---
+++ src/Nager.EmailAuthentication.UnitTest/DkimHeaderParserTests/SelectorTest.cs
@@ -0,0 +1,34 @@
+﻿namespace Nager.EmailAuthentication.UnitTest.DkimHeaderParserTests
+{
+    [TestClass]
+    public sealed class SelectorTest
+    {
+        [DataRow("selector1")]
+        [DataRow("selector2")]
+        [DataRow("google")]
+        [DataTestMethod]
+        public void TryParse_ValidVersionDkimHeaderString_ReturnsTrueAndPopulatesDataFragment(string selector)
+        {
+            var dkimHeader = $"v=1; a=rsa-sha256; d=domain.com; s={selector}; h=message-id:from; bh=testbodyhash=; b=signaturedata";
+
+            var isSuccessful = DkimHeaderParser.TryParse(dkimHeader, out var dkimHeaderDataFragment, out var parseErrors);
+
+            Assert.IsTrue(isSuccessful);
+            Assert.IsNotNull(dkimHeaderDataFragment);
+            Assert.IsNull(parseErrors, "ParseErrors is not null");
+        }
+
+        [DataRow("verylongandinvalidselectorverylongandinvalidselectorverylongandinvalidselector")]
+        [DataTestMethod]
+        public void TryParse_InvalidVersionDkimHeaderString_ReturnsTrueAndPopulatesDataFragment(string selector)
+        {
+            var dkimHeader = $"v=1; a=rsa-sha256; d=domain.com; s={selector}; h=message-id:from; bh=testbodyhash=; b=signaturedata";
+
+            var isSuccessful = DkimHeaderParser.TryParse(dkimHeader, out var dkimHeaderDataFragment, out var parseErrors);
+
+            Assert.IsTrue(isSuccessful);
+            Assert.IsNotNull(dkimHeaderDataFragment);
+            Assert.IsNotNull(parseErrors, "ParseErrors is null");
+        }
+    }
+}
